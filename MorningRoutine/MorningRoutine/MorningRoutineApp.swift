fd9871fe2855b5dcf328731ad62090343ce2c1a0
David Fumagalli <davidfumo18@gmail.com>
2022-07-05 12:22:53 -0400

    Initialized project

---
+++ MorningRoutine/MorningRoutine/MorningRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningRoutineApp.swift
+//  MorningRoutine
+//
+//  Created by David Fumagalli on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MorningRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
